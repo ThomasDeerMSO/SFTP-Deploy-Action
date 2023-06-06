32fec71078f5c54620232addd11fddf3f4a3fdf4
Thomas Deer <thomas.deer@mso.net>
2023-06-06 12:39:33 +0100

    Update entrypoint.sh to only update modified files

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,55 +13,55 @@ if [ -z "$6" ]; then
 fi
 
 # use password
-if [ -z != ${10} ]; then
+if [ -n "${10}" ]; then
 	echo 'use sshpass'
 	apk add sshpass
 
 	if test $9 == "true";then
   		echo 'Start delete remote files'
-		sshpass -p ${10} ssh -o StrictHostKeyChecking=no -p $3 $1@$2 rm -rf $6
+		sshpass -p "${10}" ssh -o StrictHostKeyChecking=no -p "$3" "$1@$2" rm -rf "$6"
 	fi
 	if test $7 = "true"; then
   		echo "Connection via sftp protocol only, skip the command to create a directory"
 	else
  	 	echo 'Create directory if needed'
- 	 	sshpass -p ${10} ssh -o StrictHostKeyChecking=no -p $3 $1@$2 mkdir -p $6
+ 	 	sshpass -p "${10}" ssh -o StrictHostKeyChecking=no -p "$3" "$1@$2" mkdir -p "$6"
 	fi
 
 	echo 'SFTP Start'
 	# create a temporary file containing sftp commands
-	printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
+	printf "%s" "put -r -d $5 $6" > "$TEMP_SFTP_FILE"
 	#-o StrictHostKeyChecking=no avoid Host key verification failed.
-	SSHPASS=${10} sshpass -e sftp -oBatchMode=no -b $TEMP_SFTP_FILE -P $3 $8 -o StrictHostKeyChecking=no $1@$2
+	SSHPASS="${10}" sshpass -e sftp -oBatchMode=no -b "$TEMP_SFTP_FILE" -P "$3" "$8" -o StrictHostKeyChecking=no "$1@$2"
 
 	echo 'Deploy Success'
 
-    exit 0
+	exit 0
 fi
 
 # keep string format
-printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
+printf "%s" "$4" > "$TEMP_SSH_PRIVATE_KEY_FILE"
 # avoid Permissions too open
-chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
+chmod 600 "$TEMP_SSH_PRIVATE_KEY_FILE"
 
 # delete remote files if needed
 if test $9 == "true";then
-  echo 'Start delete remote files'
-  ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 rm -rf $6
+	echo 'Start delete remote files'
+	ssh -o StrictHostKeyChecking=no -p "$3" -i "$TEMP_SSH_PRIVATE_KEY_FILE" "$1@$2" rm -rf "$6"
 fi
 
 if test $7 = "true"; then
-  echo "Connection via sftp protocol only, skip the command to create a directory"
+	echo "Connection via sftp protocol only, skip the command to create a directory"
 else
-  echo 'Create directory if needed'
-  ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
+	echo 'Create directory if needed'
+	ssh -o StrictHostKeyChecking=no -p "$3" -i "$TEMP_SSH_PRIVATE_KEY_FILE" "$1@$2" mkdir -p "$6"
 fi
 
 echo 'SFTP Start'
 # create a temporary file containing sftp commands
-printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
+printf "%s" "put -r -d $5 $6" > "$TEMP_SFTP_FILE"
 #-o StrictHostKeyChecking=no avoid Host key verification failed.
-sftp -b $TEMP_SFTP_FILE -P $3 $8 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
+sftp -b "$TEMP_SFTP_FILE" -P "$3" "$8" -o StrictHostKeyChecking=no -i "$TEMP_SSH_PRIVATE_KEY_FILE" "$1@$2"
 
 echo 'Deploy Success'
 exit 0
